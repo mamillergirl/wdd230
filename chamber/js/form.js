98b0214a1018cc85b84f07f175e9fac784ca2ed0
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2022-11-05 18:57:20 -0600

    add form

---
+++ chamber/js/form.js
@@ -0,0 +1,11 @@
+const date = new Date();
+const dateTime = {
+    "month": date.getMonth(),
+    "date": date.getDate(),
+    "year": date.getFullYear(),
+    "hours": date.getHours(),
+    "minutes": date.getMinutes(),
+    "seconds": date.getSeconds()
+ };
+
+document.querySelector(".time-loaded").value = dateTime;
