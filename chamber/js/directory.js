4c26c47bf8ac1810560e7afec7221a352c83ffdd
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2022-11-11 14:07:42 -0700

    photos

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -17,32 +17,57 @@ async function getBusinesses(requestURL){
 
 function displayBusiness(item){
     let card = document.createElement('section');
-    let h4 = document.createElement('h4');
+
     let logo = document.createElement('img');
     let address = document.createElement('p');
     let phone = document.createElement('p');
     let urlP = document.createElement('p');
-    let url = document.createElement('a');
-    urlP.appendChild(url);
+    
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    
+    urlP.innerHTML = `<a href="${item.url}">${item.url}</a>`
 
     logo.setAttribute('src', item.imageurl);
     logo.setAttribute('alt', item.name + " Logo");
 
-    h4.textContent = item.name;
     address.textContent = item.address;
     phone.textContent = item.phone
-    url.textContent = item.url;
 
 
-    card.appendChild(h4);
+
     card.appendChild(logo);
     card.appendChild(address);
     card.appendChild(phone);
     card.appendChild(urlP);
-    
 
+
+    tr.innerHTML = `<td>${item.name}</td><td>${item.address}</td><td>${item.phone}</td><td><a href=${item.url}>${item.url}</a></td>`
+  
     document.querySelector('.cards').appendChild(card);
-
+    document.querySelector('.list').appendChild(tr);
+    
 
 }
 getBusinesses(requestURL);
+
+function hide(which){
+    let cards = document.querySelector('.cardDisplay');
+    let list = document.querySelector('.listDisplay');
+    let hidden;
+    let displayed;
+    if (which == 'cards'){
+        hidden = cards;
+        displayed = list;
+    } else if (which == 'list'){
+        hidden = list;
+        displayed = cards;
+    }
+    hidden.style.display = 'none';
+    displayed.style.display = 'block';
+}
+
+
+
+
+
